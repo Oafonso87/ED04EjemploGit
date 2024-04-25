4d9acbd5b3c6484d9be909d6563027bda1c22ac3
Oafonso87 <151383998+Oafonso87@users.noreply.github.com>
2024-04-25 15:46:44 +0200

    Version 4

--- src/main/java/ed/ejemplogit/EjemploGit.java
+++ src/main/java/ed/ejemplogit/EjemploGit.java
@@ -9,5 +9,6 @@ public class EjemploGit {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Empezando");
+        System.out.println("Continuando");
     }
 }
