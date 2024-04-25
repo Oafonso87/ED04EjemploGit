ef69c4a962711e08a3a295a5b79d180ae9dfff13
OmarAfonso <oafonso@birt.eus>
2024-04-25 15:23:21 +0200

    Version 0. Empezando

---
+++ src/main/java/ed/ejemplogit/EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.ejemplogit;
+
+/**
+ *
+ * @author OmarAfonso
+ */
+public class EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
